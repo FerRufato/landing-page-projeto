7e6170a335b7e67ca5b1332a323c215727c5b847
Fernanda Rufato <nandaoliveira1985@terra.com.br>
2024-09-08 21:08:57 -0300

    first commit

---
+++ gulpfile.mjs
@@ -0,0 +1,47 @@
+import gulp from 'gulp';
+import gulpSass from 'gulp-sass';
+import nodeSass from 'sass'; 
+import imagemin from 'gulp-imagemin';
+import cleanCSS from 'gulp-clean-css';
+
+
+const sass = gulpSass(nodeSass);
+
+
+const paths = {
+    styles: {
+        src: 'src/scss/**/*.scss',
+        dest: 'dist/css'
+    },
+    images: {
+        src: 'src/img/**/*',
+        dest: 'dist/images'
+    }
+};
+
+
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sass().on('error', sass.logError))  
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+
+function images() {
+    return gulp.src(paths.images.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.images.dest));
+}
+
+
+function watchFiles() {
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.images.src, images);
+}
+
+
+export { styles, images, watchFiles };
+
+
+export default gulp.series(styles, images, watchFiles);
